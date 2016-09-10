503f26064a6e7dde24a57a0a41fc935a8347b96b
Ramin <ramin_yavari03@yahoo.com>
2016-09-09 17:47:52 -0700

    Added Directions for Rotate NxN Matrix

--- Other/rotateMatrix.js
+++ Other/rotateMatrix.js
@@ -1,3 +1,9 @@
+/*DIRECTIONS: 
+
+Write a function that rotates a MxN matrix 90 degrees clockwise.
+
+*/
+
 let rotateMbyN = (matrix) => {
   let rotated = [];
   let rows = matrix[0].length;
@@ -13,4 +19,37 @@ let rotateMbyN = (matrix) => {
   return rotated;
 }
 
-//____________________________________________
+//_____________________________________________________
+
+/*
+
+DIRECTIONS: 
+
+Write a function that rotates a NxN matrix 90 degrees clockwise.
+A matrix, also called a 2-D array, is simply an array of arrays of values.
+
+2x2 MATRIX EXAMPLE:
+
+[ [1, 2],
+[3, 4] ]
+
+Rotated:
+
+[ [3, 1],
+[4, 2] ]
+
+4x4 MATRIX EXAMPLE:
+
+[ [ 1, 2, 3, 4],
+[ 5, 6, 7, 8],
+[ 9, “A”,”B”,”C”],
+[“D”,”E”,”F”,”G”] ]
+
+Rotated:
+
+[ [“D”, 9, 5, 1],
+[“E”, “A”, 6, 2],
+[“F”,”B”, 7, 3],
+[“G”,”C”, 8, 4] ]
+*/
+
