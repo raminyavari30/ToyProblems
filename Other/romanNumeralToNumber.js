4d1bb31f002f5acdfe3f8f5169caabdfe7e08e66
Ramin <ramin_yavari03@yahoo.com>
2016-09-18 14:57:28 -0700

    Alternative Solution to RomanNumeralsToNumber

--- Other/romanNumeralToNumber.js
+++ Other/romanNumeralToNumber.js
@@ -41,3 +41,41 @@ const romanToNumber = (rom) => {
   }
   return result;
 }
+
+//Alternative Solution:
+
+const romanToNum = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100
+}
+
+const upper = (str) => {
+  return str.toUpperCase();
+}
+
+const RomanNumeralsToNumber = (rom) => {
+  
+  if(!rom) {
+    return 0;
+  }
+  
+  let result = 0;
+  let prev = 0;
+  let current;
+  
+  for(let i = rom.length-1; i >=0; i--) {
+    current = romanToNum[upper(rom[i])];
+    
+      if(current >= prev) {
+        result+=current;
+      }
+      else {
+        result-=current;
+      }
+      prev = current;
+  }
+  return result;
+}
